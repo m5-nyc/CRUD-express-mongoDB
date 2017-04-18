3058bf103f2a475d0667cbbb75e6a7bbbd307c30
Christopher Maverick <cmaverickce@gmail.com>
2017-04-18 15:58:08 -0400

    CRUD - create

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
+
+// app.use method is for middlewares
+// place before handlers
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
     console.log('listening on 3000')
@@ -8,4 +12,8 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
