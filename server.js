0f787700e50115b25a92a6a6c534933b6afab6a3
Christopher Maverick <cmaverickce@gmail.com>
2017-04-19 09:13:57 -0400

    mongodb setup

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const linkToMongodb = 'mongodb://maverick:maverick5@ds139989.mlab.com:39989/star-wars-quotes'
+
+var db;
+MongoClient.connect(linkToMongodb, (err, database) => {
+  if(err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+ })
+})
 
 // app.use method is for middlewares
 // place before handlers
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.listen(3000, () => {
-    console.log('listening on 3000')
-})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    db.collection('quotes').save(req.body, (err, result) => {
+      if(err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+    })
 })
